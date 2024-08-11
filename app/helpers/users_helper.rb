f866f387f1b196f56727399f2f271e194ddfc98a
Mauro Sánchez <msanchez@shibumi.cl>
2024-08-11 14:15:35 -0400

    fix

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,2 @@
 module UsersHelper
-  before_save :downcase_email
-
-  def downcase_email
-    email.downcase!
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, uniqueness: true
   validates :username, uniqueness: true
+  before_save :downcase_email
+
+  def downcase_email
+    email.downcase!
+  end
 end
