fd3f868ae3eda560152c90b649ed5eca8e487d19
Mauro Sánchez <msanchez@shibumi.cl>
2024-08-14 10:31:23 -0400

    wip

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -22,4 +22,120 @@ RSpec.describe Question, type: :model do
       end
     end
   end
+
+  describe 'Checking answers for Ivysaur' do
+    let(:pokemon) { Pokemon.find 2 }
+    context 'Answer for ability' do
+      it 'Bad answer should returns false' do
+        expect(Question.check_answer(pokemon, 'ability', 'Berserk')).to eq(false)
+      end
+      it 'Correct answer should returns true' do
+        expect(Question.check_answer(pokemon, 'ability', 'Overgrow')).to eq(true)
+      end
+    end
+    context 'Answer for type' do
+      it 'Bad answer should returns false' do
+        expect(Question.check_answer(pokemon, 'type', 'Electric')).to eq(false)
+      end
+      it 'Correct answer should returns true' do
+        expect(Question.check_answer(pokemon, 'type', 'Grass')).to eq(true)
+      end
+    end
+    context 'Answer for name' do
+      it 'Bad answer should returns false' do
+        expect(Question.check_answer(pokemon, 'name', 'Charizard')).to eq(false)
+      end
+      it 'Correct answer should returns true' do
+        expect(Question.check_answer(pokemon, 'name', 'Ivysaur')).to eq(true)
+      end
+    end
+    context 'Answer for evolves to' do
+      it 'Bad answer should returns false' do
+        expect(Question.check_answer(pokemon, 'evolves_to', 'Pikachu')).to eq(false)
+      end
+      it 'Correct answer should returns true' do
+        expect(Question.check_answer(pokemon, 'evolves_to', 'Venusaur')).to eq(true)
+      end
+    end
+    context 'Answer for evolves from' do
+      it 'Bad answer should returns false' do
+        expect(Question.check_answer(pokemon, 'evolves_from', 'Feraligatr')).to eq(false)
+      end
+      it 'Correct answer should returns true' do
+        expect(Question.check_answer(pokemon, 'evolves_from', 'Bulbasaur')).to eq(true)
+      end
+    end
+    context 'Answer for height' do
+      it 'Bad answer should returns false' do
+        expect(Question.check_answer(pokemon, 'height', '1.1 m')).to eq(false)
+      end
+      it 'Correct answer should returns true' do
+        expect(Question.check_answer(pokemon, 'height', '1 m')).to eq(true)
+      end
+    end
+    context 'Answer for weight' do
+      it 'Bad answer should returns false' do
+        expect(Question.check_answer(pokemon, 'weight', '21 kg')).to eq(false)
+      end
+      it 'Correct answer should returns true' do
+        expect(Question.check_answer(pokemon, 'weight', '13 kg')).to eq(true)
+      end
+    end
+    context 'Answer for flavor_text' do
+      it 'Bad answer should returns false' do
+        expect(Question.check_answer(pokemon, 'flavor_text', 'False flavor text')).to eq(false)
+      end
+      it 'Correct answer should returns true' do
+        expect(Question.check_answer(pokemon, 'flavor_text', pokemon.flavor_text)).to eq(true)
+      end
+    end
+    context 'Answer for Pokédex number' do
+      it 'Bad answer should returns false' do
+        expect(Question.check_answer(pokemon, 'pokedex_number', 1)).to eq(false)
+      end
+      it 'Correct answer should returns true' do
+        expect(Question.check_answer(pokemon, 'pokedex_number', 2)).to eq(true)
+      end
+    end
+    context 'Answer for category' do
+      it 'Bad answer should returns false' do
+        expect(Question.check_answer(pokemon, 'category', 'Fire Lizard')).to eq(false)
+      end
+      it 'Correct answer should returns true' do
+        expect(Question.check_answer(pokemon, 'category', 'Seed Pokémon')).to eq(true)
+      end
+    end
+  end
+
+  describe "Answers for Pokémon that doesn't evolve" do
+    let(:pokemon) { Pokemon.find 250 }
+    context 'Answer for evolves to' do
+      it 'Bad answer should returns false' do
+        expect(Question.check_answer(pokemon, 'evolves_to', 'Raichu')).to eq(false)
+      end
+      it 'Correct answer should returns true' do
+        expect(Question.check_answer(pokemon, 'evolves_to', "Doesn't evolve")).to eq(true)
+      end
+    end
+    context 'Answer for evolves from' do
+      it 'Bad answer should returns false' do
+        expect(Question.check_answer(pokemon, 'evolves_from', 'Ponyta')).to eq(false)
+      end
+      it 'Correct answer should returns true' do
+        expect(Question.check_answer(pokemon, 'evolves_from', "Doesn't have pre-evolution")).to eq(true)
+      end
+    end
+  end
+
+  describe 'Answers for Pokémon that is second evolution' do
+    let(:pokemon) { Pokemon.find 3 }
+    context 'Answer for evolves from' do
+      it 'Bad answer should returns false' do
+        expect(Question.check_answer(pokemon, 'evolves_from', 'Igglybuff')).to eq(false)
+      end
+      it 'Correct answer should returns true' do
+        expect(Question.check_answer(pokemon, 'evolves_from', 'Ivysaur')).to eq(true)
+      end
+    end
+  end
 end
--- spec/requests/quizzes_spec.rb
+++ spec/requests/quizzes_spec.rb
@@ -1,12 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe 'Quizzes', type: :request do
+  let(:user) { FactoryBot.create(:user) }
   describe 'GET /new' do
-    let(:the_request) { get '/quizzes/new' }
     context 'When not authenticated' do
       it 'Should redirect' do
-        the_request
+        get '/quizzes/new'
         expect(response).to redirect_to new_session_path
+      end
+    end
+  end
+
+  describe 'Post new Quiz' do
+    context 'When not authenticated' do
+      it 'Should redirect' do
+        post '/quizzes'
+        expect(response).to redirect_to new_session_path
+      end
+    end
+    context 'Authenticated, valid pokemon' do
+      before do
+        post '/sessions/', params: { email: user.email, password: user.password }
+      end
+
+      it "Shouldn't redirect" do
+        post('/quizzes', params: { "pokemon_id": 1, question: { "category": 'Seed Pokémon' } })
+        Rails.logger.debug "RESPONSE: #{response}"
+        expect(response).to redirect_to quiz_quiz_result_path(Quiz.last)
       end
     end
   end
